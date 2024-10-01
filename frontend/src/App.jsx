b07053b0a2b298fa8d7c77e0cc4313fd1fd2c571
Trupesh Savaliya <adtechifyinfotech@gmail.com>
2024-10-01 07:38:29 +0530

    "Refactor auth controller, middleware, and utils; update frontend store and App component"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
 import { RouterProvider } from "react-router-dom";
 import NetflixCloneRouter from "./routes";
 import "./assets/css/style.css";
+import { useAuthStore } from "./store";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Footer } from "./components";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <RouterProvider router={NetflixCloneRouter} />;
+  const { isCheckingAuth, authCheck } = useAuthStore();
+
+  useEffect(() => {
+    authCheck();
+  }, [authCheck]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10" />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <RouterProvider router={NetflixCloneRouter} />;
+      <Footer />
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
